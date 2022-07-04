2be6d59c6d09ad6b15cf77317d6ecc7debe369ba
Kelly Duran <kaduran16@gmail.com>
2022-07-04 12:41:49 -0400

    commit tree e73df8f95bd5ad8683a00970efdbc5a958b2a7ca

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(userName, userEmail){
+    return(
+        fetch(`http://localhost:3000/users`,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body:JSON.stringify({
+                name:userName,
+                email:userEmail
+            })
+        }).then((res) => res.json()).then((result)=>{
+            console.log(result)
+            const userID = result.id
+            console.log(userID) 
+            //document.createElement("p") -- this is in memory only
+            const userIdPTag = document.createElement("p")
+            userIdPTag.textContent = userID
+            document.body.append(userIdPTag)
+
+        }).catch(function(error){
+            console.log(error.message),
+            document.body.append(error.message)
+
+        })
+
+    )
+}
